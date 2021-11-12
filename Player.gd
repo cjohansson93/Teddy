2da3c51ac24c3568e9e0bc63ecd2ff167de17f86
cjohansson93 <63963568+cjohansson93@users.noreply.github.com>
2021-11-11 21:44:18 -0500

    Implemented scene transition

--- Player.gd
+++ Player.gd
@@ -19,6 +19,7 @@ onready var animationState = animationTree.get("parameters/playback")
 
 func _ready():
 	animationTree.active = true
+	self.global_position = Global.player_initial_map_position
 
 func _physics_process(delta):
 	match state:
